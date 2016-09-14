854ce9350f7214581febbbe4bc12a70f58a77afb
wrflemin <wrflemin@gmail.com>
2016-09-14 15:18:01 -0600

    Made the server a proxy

--- lab2.py
+++ lab2.py
@@ -3,7 +3,7 @@ import socket
 # Alocate new socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Make a connection
 server.bind(('0.0.0.0', 8000))
 server.listen(1)
@@ -12,3 +12,28 @@ print "Waiting for connections . . ."
 client, address = server.accept()
 print "Connected!"
 print address
+
+outgoing = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+outgoing.connect(("www.google.ca", 80))
+outgoing.setblocking(0)
+client.setblocking(0)
+
+while True:
+    try:
+        part = client.recv(1024)
+    except socket.error, exception:
+        if exception.errno == 11:
+            part = None
+        else:
+            raise
+    if (part):
+        outgoing.sendall(part)
+        print "< " + part
+    try:
+        part = outgoing.recv(1024)
+    except socket.error, exception:
+        if exception.errno == 11:
+            part = None
+    if(part):
+        client.sendall(part)
+        print "> " + part
